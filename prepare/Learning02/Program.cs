a8c54e09bbb5fed23327a86e081a6742925012dd
shoe-eater <scottjose255pl@byui.edu>
2026-05-05 10:40:21 -0600

    Prepare - Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._companyName = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Game Engine Developer";
+        job2._companyName = "Nintendo";
+        job2._startYear = 2022;
+        job2._endYear = 2026;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Scott Raber";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.DisplayResumeDetails();
     }
 }
