5b79460f10a15e845f42bb30ce6dd4ab920c2469
shoe-eater <scottjose255pl@byui.edu>
2026-05-19 12:50:10 -0600

    Learning 03

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,52 @@
+using System.Globalization;
+
+class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    // Constructors:
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+
+    // Getters/Setters
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+    public void SetDenominator(int denominator)
+    {
+        _denominator = denominator;
+    }
+
+    // Return functions
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Fraction fraction1 = new Fraction();
+        // Fraction fraction2 = new Fraction(6);
+        // Fraction fraction3 = new Fraction(6, 7);
+
+        // fraction1.SetDenominator(4);
+        // Console.WriteLine(fraction1.GetDenominator());
+        // fraction2.SetNumerator(3);
+        // Console.WriteLine(fraction2.GetNumerator());
+
+        // Console.WriteLine(fraction1.GetFractionString());
+        // Console.WriteLine(fraction1.GetDecimalValue());
+        // Console.WriteLine(fraction2.GetFractionString());
+        // Console.WriteLine(fraction2.GetDecimalValue());
+        // Console.WriteLine(fraction3.GetFractionString());
+        // Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction randomFraction = new Fraction();
+        Random randomNumerator = new Random();
+        Random randomDenominator = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            randomFraction.SetNumerator(randomNumerator.Next());
+            randomFraction.SetDenominator(randomDenominator.Next());
+            Console.WriteLine(randomFraction.GetFractionString());
+            Console.WriteLine(randomFraction.GetDecimalValue());
+        }
     }
 }
