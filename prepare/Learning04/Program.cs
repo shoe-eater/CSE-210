3a302fe4ccfaabfe01528fa5d5b6f7f52e15f718
Scott Raber <scottjose255pl@byui.edu>
2026-06-02 21:40:15 -0600

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment myAssignment = new Assignment("Scott Raber", "Calculus");
+        Console.WriteLine(myAssignment.GetSummary());
+
+        MathAssignment myMathAssignment = new MathAssignment("Scott Raber", "Introduction to Analysis", "1.7", "1-3;8");
+        Console.WriteLine(myMathAssignment.GetHomeworkList());
+
+        WritingAssignment myWritingAssignment = new WritingAssignment("Scott Raber", "Creative Writing", "Tales of Soris");
+        Console.WriteLine(myWritingAssignment.GetWritingInformation());
     }
 }
